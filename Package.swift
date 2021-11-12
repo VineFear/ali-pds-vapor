922f768b0c196618c83029533ee7b18e9d84af98
vine <1953892532@qq.com>
2021-11-13 00:19:17 +0800

    feat: update async

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "ali-pds-vapor",
     platforms: [
-        .macOS(.v10_15),
+        .macOS(.v12),
         .iOS(.v14)
     ],
     products: [
@@ -19,7 +19,7 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0"),
-        .package(url: "https://github.com/VineFear/ali-pds-kit.git", from: "0.0.1")
+        .package(url: "https://github.com/VineFear/ali-pds-kit.git", .branch("develop"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
--- Sources/Run/configure.swift
+++ Sources/Run/configure.swift
@@ -1,7 +1,12 @@
 import Vapor
 import AliPdsVapor
+
 func configure(_ app: Application) throws {
     // MARK: Config AliPDS
+    /*
+     touch .env
+     echo "ALI_PDS_SECRET=bc5dfesadfsfsfca8f64086dd9ea2ac" >> .env
+     */
     if let pdsSecret = Environment.get("ALI_PDS_SECRET") {
         app.aliPds.credentials = .init(credentials: .init(secret: pdsSecret))
     }
@@ -11,7 +16,25 @@ func configure(_ app: Application) throws {
 }
 
 func routes(_ app: Application) throws {
+    
     app.get { req -> String in
         return "Hello, world!"
     }
+    
+    // http://127.0.0.1:8080/list/59000000?parentId=root
+    app.get("list", ":driveId") { req async throws -> FileDriveModel in
+        
+        guard let driveId = req.parameters.get("driveId") else {
+            throw Abort(.badRequest)
+        }
+        
+        guard let parentfileId = req.query[String.self, at: "parentId"] else {
+            throw Abort(.notFound, reason: "Please input parentId")
+        }
+        
+        let fileDriveModel = try await req.driveClient.drive.getFileList(driveId: driveId, parentFileId: parentfileId)
+        return fileDriveModel
+    }
 }
+
+extension FileDriveModel: Content { }
